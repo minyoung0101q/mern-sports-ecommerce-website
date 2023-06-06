d4764170822a5c00bf7c954fee9dd353b731ddf6
stylish_code <130943387+minyoung0101q@users.noreply.github.com>
2023-06-06 16:27:16 +0900

    Server-code

---
+++ Server/data/Products.js
@@ -0,0 +1,64 @@
+const products = [
+  {
+    name: "BES ACG 로우케이트",
+    image: "/images/1.png",
+    description:
+      "거리에서 공원을 지나 트레일까지, 클래식한 도시와 모험을 위한 신발을 신고 달려보세요. 거친 태평양 북서부에서 디자인되고 테스트를 거친 혼합 소재로 제작된 갑피는 내구성과 간편한 스타일링이 조화를 이룹니다. 고무 밑창(견고하고 조율된 러그 솔 패턴)은 매끄럽고 바위가 많은 지면에서 그립력을 높여 주어 위아래로 움직일 수 있습니다.",
+    price: 129000,
+    countInStock: 10,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "BES 라이프 써마 핏",
+    image: "/images/4.png",
+    description:
+      "이 단열 패딩은 앞으로 나아가면서 가장 추운 날씨에도 거리를 걸으며 산뜻한 룩을 연출하기 위해 필요한 모든 보온성을 제공합니다. 두꺼운 Thermore® EcoDown® 충전재 패널에 넉넉한 후드와 스톰 플랩을 더해 궂은 날씨로부터 몸을 보호해 줍니다.",
+    price: 369000,
+    countInStock: 200,
+    rating: 5,
+    numReviews: 2,
+  },
+  {
+    name: "BES 에어맥스 97",
+    image: "/images/2.png",
+    description:
+      "역사책 속으로 입장하세요. 이 스페셜 에디션은 에어 쿠셔닝의 초석을 세운 프랭크 루디에게 경의를 표합니다. 시스루 'Air Pocket' 힐 로고부터 에어 유닛의 멋진 'Est. 1997' 스탬핑에 이르기까지, 편안한 착화감과 재미있는 디테일로 메시지를 전하며 프랭크의 레거시를 기념합니다.",
+    price: 219000,
+    countInStock: 100,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "BES 스포츠웨어 에센셜 리펠",
+    image: "/images/5.png",
+    description:
+      "BES 스포츠웨어 에센셜 리펠 재킷은 매끄러운 발수 기능성 소재로 제작되어 비 오는 쌀쌀한 날에도 한결같이 쾌적하고 편안합니다. 신축성 있는 커프스가 소매를 제자리에 고정해 주며, 밑단의 노출형 번지 코드로 원하는 스타일과 핏을 연출할 수 있습니다. 이 제품은 100% 재생 폴리에스터 섬유로 제작되었습니다.",
+    price: 89000,
+    countInStock: 569,
+    rating: 5,
+    numReviews: 66,
+  },
+  {
+    name: "조던 .5 'Why Not?'",
+    image: "/images/3.png",
+    description:
+      "러셀 웨스트브룩은 빠릅니다. 이 신발은 새로워진 쿠셔닝, 데이터 기반 접지력, 발 전체를 감싸는 핏 시스템으로 러셀이 빠른 속도에서도 제어력을 발휘하도록 합니다. 컬러와 소재가 충돌하도록 의도적으로 매치한 다부진 유틸리티 룩의 이번 모델은 러셀의 만트라인 'Why Not?' 정신을 담아냅니다.",
+    price: 159000,
+    countInStock: 80,
+    rating: 4.5,
+    numReviews: 40,
+  },
+  {
+    name: "조던 23 엔지니어드",
+    image: "/images/6.png",
+    description:
+      "훌륭하게 제작된 편안한 레이어링. 여유 있는 사이즈로 제작된 23 엔지니어드 후디는 매우 무게감 있는 플리스로 제작되었으며, 앞면 지퍼 포켓이 있어 소지품을 보관할 수 있습니다.",
+    price: 105000,
+    countInStock: 726,
+    rating: 5,
+    numReviews: 9,
+  },
+];
+
+export default products;
